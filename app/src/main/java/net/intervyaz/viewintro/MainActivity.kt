5d910cbd310633f77a0429418ce418b24ca24551
xkxsxux <simonova278@gmail.com>
2021-09-21 01:23:16 +0500

    ..

--- app/src/main/java/net/intervyaz/viewintro/MainActivity.kt
+++ app/src/main/java/net/intervyaz/viewintro/MainActivity.kt
@@ -3,7 +3,9 @@ package net.intervyaz.viewintro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import net.intervyaz.viewintro.databinding.ActivityMainBinding
+import net.intervyaz.viewintro.databinding.FragmentSettingsBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +19,21 @@ class MainActivity : AppCompatActivity() {
         val adapter = TicketsAdapter(this, fakeTickets)
         binding.recycler.adapter = adapter
         binding.recycler.layoutManager = LinearLayoutManager(this)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.selectedItemId = R.id.fragment_second
+        bottomNavigation.setOnNavigationItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    // Respond to navigation item 1 reselection
+                }
+                R.id.item_2 -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view, FragmentSecond()).commit()
+                    val fragment = FragmentSecond()
+                    val transaction = supportFragmentManager?.beginTransaction()
+                    transaction?.replace(R.id.fragment_container_view, fragment)
+                    transaction?.commit()
+                }
+            }
+        }
     }
 }
