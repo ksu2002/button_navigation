c196beef02ce5c2c3fb08ca835eae3fb3680fcf1
xkxsxux <simonova278@gmail.com>
2021-09-21 00:38:58 +0500

    ..

---
+++ app/src/main/java/net/intervyaz/viewintro/FragmentSettings.kt
@@ -0,0 +1,15 @@
+package com.example.fragments
+
+import android.R.attr
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import net.intervyaz.viewintro.R
+
+
+class FragmentSecond : Fragment(R.layout.fragment_settings) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
